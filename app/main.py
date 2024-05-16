ced6c7b348495003f1872ed4eef2e5667e42e0f1
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-15 21:18:39 -0700

    🚧 Add new endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 import json
+import os
+from openai import OpenAI
 from fastapi import FastAPI
 
+chatGPT_key = os.environ["chatGPT_key"]
 
+
+def init_client(api_key):
+    return OpenAI(api_key=api_key)
+
+
+client = init_client(chatGPT_key)
+promp = "haz una nota deportiva de 5 párrafos de un partido de la tercera jornada del torneo de fútbol sala FutSal NIES. Los equipos fueron Eco Pasto contra Cachorros. El marcador quedó 5 a 4 a favor de Eco Pasto. Los anotadores por Eco Pasto fueron Jared (3 goles), Saul (1 gol) y Jonathan (1 gol). Los goles de los Cachorros fueron de Jesús Trujillo (3 goles) y Adán Gómez"
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo", messages=[{"role": "user", "content": promp}]
+)
 f = open("./data/fixtures.json")
 fixtures = json.load(f)
 app = FastAPI(
-    title="API of NIES",
+    title="API of FutSal NIES",
     description="API made by NIES",
     summary="API about data of using by NIES",
     version="0.1.0",
@@ -24,3 +37,8 @@ app = FastAPI(
 @app.get("/v1/fixtures/")
 def get_fixture():
     return fixtures
+
+
+@app.get("/v1/match_note/")
+def get_match_note():
+    return print(response)
